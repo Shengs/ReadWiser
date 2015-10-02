b45eb7b4225e4eecb702724a2cff451b8343de6c
Shiheng Zhang <shawn.zsh@gmail.com>
2015-10-02 00:33:53 -0700

    testing

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-'use strict';
+// 'use strict';
 
-var express = require('express');
-var kraken = require('kraken-js');
-var db = require('./lib/db');
-var flash = require('connect-flash');
-
-
-var options, app;
-
-/*
- * Create and configure application. Also exports application instance for use by tests.
- * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
- */
-options = {
-    onconfig: function (config, next) {
-        /*
-         * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
-         */
-        db.config(config.get('databaseConfig'));
-        next(null, config);
-    }
-};
-
-app = module.exports = express();
-app.use(kraken(options));
-
-// Setup Flash
-app.use(flash());
-app.use(function (req,res,next) {
-    res.locals.messages = require('express-messages')(req,res);
-    next();
-});
-
-app.on('start', function () {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
-});
+// var express = require('express');
+// var kraken = require('kraken-js');
+// var db = require('./lib/db');
+// var flash = require('connect-flash');
 
 
-// // Setup Server
-
-// var server;
+// var options, app;
 
 // /*
-//  * Create and start HTTP server.
+//  * Create and configure application. Also exports application instance for use by tests.
+//  * See https://github.com/krakenjs/kraken-js#options for additional configuration options.
 //  */
+// options = {
+//     onconfig: function (config, next) {
+//         /*
+//          * Add any additional config setup or overrides here. `config` is an initialized
+//          * `confit` (https://github.com/krakenjs/confit/) configuration object.
+//          */
+//         db.config(config.get('databaseConfig'));
+//         next(null, config);
+//     }
+// };
 
-// server = http.createServer(app);
-// server.listen(process.env.PORT || 8000);
-// server.on('listening', function () {
-//     console.log('Server listening on http://localhost:%d', this.address().port);
+// app = module.exports = express();
+// app.use(kraken(options));
+
+// // Setup Flash
+// app.use(flash());
+// app.use(function (req,res,next) {
+//     res.locals.messages = require('express-messages')(req,res);
+//     next();
 // });
+
+// app.on('start', function () {
+//     console.log('Application ready to serve requests.');
+//     console.log('Environment: %s', app.kraken.get('env:env'));
+// });
+
+
+// // // Setup Server
+
+// // var server;
+
+// // /*
+// //  * Create and start HTTP server.
+// //  */
+
+// // server = http.createServer(app);
+// // server.listen(process.env.PORT || 8000);
+// // server.on('listening', function () {
+// //     console.log('Server listening on http://localhost:%d', this.address().port);
+// // });
